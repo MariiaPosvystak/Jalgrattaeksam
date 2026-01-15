fbb1ddbbae6fe33f4f78e117210cac1d6550d3c5
Mariia Posvystak <posvystakmariia@gmail.com>
2026-01-15 14:15:59 +0200

    fail nav on loodatud

---
+++ nav.php
@@ -0,0 +1,10 @@
+<nav class="nav">
+    <ul>
+        <li><a href="registreerimine.php">Registration</a></li>
+        <li><a href="Teooriaeksam.php">Teooria eksam</a></li>
+        <li><a href="Slaalom.php">Slaalom</a></li>
+        <li><a href="Ringtee.php">Ringtee</a></li>
+        <li><a href="Tanav.php">Tänavasõit</a>
+        <li><a href="Lubadeleht.php">Lubadeleht</a>
+    </ul>
+</nav>
