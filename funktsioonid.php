a3f9fc19fcd7f63b4bc58b48f480ead2984f99a5
Mariia Posvystak <posvystakmariia@gmail.com>
2026-01-15 15:08:37 +0200

    Funktsioon naitaTabel (teooriatulemus) on loodatud

--- funktsioonid.php
+++ funktsioonid.php
@@ -17,4 +17,26 @@ function teooriatulemus($id, $teooriatulemus)
     $stmt->execute();
     $stmt->close();
 }
+function naitaTabel() {
+    global $connect;
+    $paring = $connect->prepare("SELECT id, eesnimi, perekonnanimi FROM jalgrattaeksam WHERE teooriatulemus<10");
+    $paring->bind_result($id, $eesnimi, $perekonnanimi);
+    $paring->execute();
+    while ($paring->fetch()) {
+        echo "<table>";
+        echo "<tr>
+    <td>$eesnimi</td>
+    <td>$perekonnanimi</td>
+    <td>
+    <form method='post' action=''>
+    <input type='hidden' name='id' value='$id' />
+    <input type='text' name='teooriatulemus' />
+    <input type='submit' value='Sisesta tulemus' />
+    </form>
+    </td>
+    </tr>";
+        echo "</table>";
+    }
+    $paring->close();
+}
 ?>
