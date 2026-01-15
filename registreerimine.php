8f235ed354feba2e095fb3b22fd4e4753fece1ef
Mariia Posvystak <posvystakmariia@gmail.com>
2026-01-15 14:53:10 +0200

    lisatud nimi kontroll

--- registreerimine.php
+++ registreerimine.php
@@ -1,11 +1,29 @@
 <?php
 require_once("konf.php");
 global $connect;
-if(isSet($_REQUEST["sisestusnupp"])){
-    $kask=$connect->prepare(
-        "INSERT INTO jalgrattaeksam(eesnimi, perekonnanimi) VALUES (?, ?)"); $kask->bind_param("ss", $_REQUEST["eesnimi"], $_REQUEST["perekonnanimi"]); $kask->execute();
-    $connect->close();
-    header("Location: $_SERVER[PHP_SELF]?lisatudeesnimi=$_REQUEST[eesnimi]"); exit();
+if (!empty($_POST["sisestusnupp"])) {
+    if (isset($_POST["eesnimi"])) {
+        $eesnimi = trim($_POST["eesnimi"]);
+    } else {
+        $eesnimi = '';
+    }
+
+    if (isset($_POST["perekonnanimi"])) {
+        $perekonnanimi = trim($_POST["perekonnanimi"]);
+    } else {
+        $perekonnanimi = '';
+    }
+
+    if ($eesnimi === '' || is_numeric($eesnimi)) {
+        echo "Sisesta oma eesnimi!";
+    } elseif ($perekonnanimi === '' || is_numeric($perekonnanimi)) {
+        echo "Sisesta oma perekonnanimi!";
+    } else {
+        $stmt = $connect->prepare("INSERT INTO jalgrattaeksam (eesnimi, perekonnanimi) VALUES (?, ?)");
+        $stmt->bind_param("ss", $eesnimi, $perekonnanimi);
+        $stmt->execute();
+        $connect->close();
+    }
 }
 ?>
 <!doctype html>
@@ -19,13 +37,9 @@ if(isSet($_REQUEST["sisestusnupp"])){
 include("header.php");
 include("nav.php");
 ?>
+<main>
 <h1>Registreerimine</h1>
-<?php
-if(isSet($_REQUEST["lisatudeesnimi"])){
-    echo "Lisati $_REQUEST[lisatudeesnimi]";
-}
-?>
-<form action="?">
+<form action="?" method="post">
     <dl>
         <dt>Eesnimi:</dt>
         <dd><input type="text" name="eesnimi" /></dd>
@@ -33,6 +47,7 @@ if(isSet($_REQUEST["lisatudeesnimi"])){
         <dd><input type="text" name="perekonnanimi" /></dd>
         <dt><input type="submit" name="sisestusnupp" value="sisesta" /></dt>  </dl>
 </form>
+</main>
 <?php
 include("footer.php");
 ?>
