cde0179fa14e32ef7a43aa0742bcb670527b0b18
Mariia Posvystak <posvystakmariia@gmail.com>
2026-01-15 15:04:06 +0200

    Funktsioon teooriatulemus on loodatud

--- funktsioonid.php
+++ funktsioonid.php
@@ -9,4 +9,12 @@ function lisaKasutaja($eesnimi, $perekonnanimi)
     $stmt->execute();
     $stmt->close();
 }
+function teooriatulemus($id, $teooriatulemus)
+{
+    global $connect;
+    $stmt = $connect->prepare("UPDATE jalgrattaeksam SET teooriatulemus=? WHERE id=?");
+    $stmt->bind_param("ii", $teooriatulemus, $id);
+    $stmt->execute();
+    $stmt->close();
+}
 ?>
